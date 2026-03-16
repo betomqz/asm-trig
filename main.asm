c34020805f521e7085cca81b8b9c2f26911e75db
betomqz <jmarquezlujan@gmail.com>
2026-03-15 23:28:55 -0600

    add a menu

--- main.asm
+++ main.asm
@@ -10,36 +10,114 @@ EXTERN TablaGrados:PROC
 
 
 .DATA
-adios           BYTE 0dh,0ah,"ADIOS.",0
+adios           BYTE 0dh,0ah,"Adiós.",0
 
 PUBLIC strComa
 strComa         BYTE ", ",0
 
 PUBLIC strDivOut
-strDivOut       BYTE "----------------------------------------"
-                BYTE "----------------------------------------",0
+strDivOut       BYTE 80 DUP("-"),0
 
 .CODE
 
 ; -------------------------------------------------------------------------------
+; main
+;   Procedimiento principal. Imprime el menú y ejecuta la acción indicada.
+;
+; Recibe: nada
+; Devuelve: nada. Escribe en la consola.
+; -------------------------------------------------------------------------------
 main PROC
     finit                       ; inicializa el FPU
 
-    ; Pide radianes y calcula
-    CALL PideRadianes
+    MOV  EAX, 0
+    .WHILE EAX == 0
 
-    ; Pide grados y calcula
-    CALL PideGrados
+        ; --- Imprime el menú ---
+        CALL ImprimeMenu
 
-    ; Imprime tabla
-    CALL TablaGrados
+        ; ---- Lee entrada ---
+        CALL ReadInt                ; EAX = valor leído
+
+        ; --- Ejecuta acción seleccionada ---
+        .IF EAX == 1
+            CALL PideGrados
+            MOV  EAX, 0
+        .ELSEIF EAX == 2
+            CALL PideRadianes
+            MOV  EAX, 0
+        .ELSEIF EAX == 3
+            CALL TablaGrados
+            MOV  EAX, 0
+        .ELSE
+            ; --- Rompe el loop ---
+            MOV  EAX, 1
+        .ENDIF
+    .ENDW
 
     ; Despedida
-    mov  edx, OFFSET adios
-    call WriteString
-    call CrLf
+    MOV  EDX, OFFSET adios
+    CALL WriteString
+    CALL CrLf
 
     EXIT
 main ENDP
+
+
+; -------------------------------------------------------------------------------
+; ImprimeMenu
+;   Procedimiento para imprimir el menú de opciones.
+;
+; Recibe: nada
+; Devuelve: nada. Escribe en la consola.
+; -------------------------------------------------------------------------------
+.DATA
+strInstruct      BYTE "Ingrese el número correspondiente a la acción que desea ejecutar:",0
+strOpGrados      BYTE "1: Lectura de x en grados",0
+strOpRadianes    BYTE "2: Lectura de x en radianes",0
+strOpTabla       BYTE "3: Generación de una tabla desde 0 a 360 grados",0
+strOpExit        BYTE "4: Salir",0
+strMenuHeader    BYTE 37 DUP("#"),0
+strMenuWord      BYTE " MENÚ ",0
+strPrompt        BYTE "input> ",0
+
+.CODE
+ImprimeMenu PROC
+    ; --- Imprime el header ---
+    CALL CrLf
+    MOV  EDX, OFFSET strMenuHeader
+    CALL WriteString
+    MOV  EDX, OFFSET strMenuWord
+    CALL WriteString
+    MOV  EDX, OFFSET strMenuHeader
+    CALL WriteString
+    CALL CrLf
+
+    ; --- Imprime las instrucciones ---
+    MOV  EDX, OFFSET strInstruct
+    CALL WriteString
+    CALL CrLf
+
+    ; --- Imprime las opciones ---
+    MOV  EDX, OFFSET strOpGrados
+    CALL WriteString
+    CALL CrLf
+    MOV  EDX, OFFSET strOpRadianes
+    CALL WriteString
+    CALL CrLf
+    MOV  EDX, OFFSET strOpTabla
+    CALL WriteString
+    CALL CrLf
+    MOV  EDX, OFFSET strOpExit
+    CALL WriteString
+    CALL CrLf
+
+    ; --- Imprime prompt ---
+    MOV  EDX, OFFSET strPrompt
+    CALL WriteString
+
+    RET
+
+ImprimeMenu ENDP
 
 END main
